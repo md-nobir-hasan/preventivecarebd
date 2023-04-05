6df4c4d496e98cb4a545961c52edfd000048aa91
Md. Nobir Hasan <nobir.wd@gmail.com>
2023-04-05 15:53:40 +0600

    fdf

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -3,16 +3,7 @@
 <html dir="ltr" lang="bn">
 
     {{-- Header  --}}
-    <head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-        <title>{{ $site_info->title ?? env('APP_NAME') }} </title>
-        <base />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <link href="{{ $site_info->logo }}" rel="icon" />
-        <link rel="stylesheet" href="{{ asset('/css/style.css') }}">
-        @stack('custom-css')
-    </head>
+    @include('frontend.partials.header')
 
 <body class="common-home">
     <div class="toastr-div">
@@ -22,7 +13,6 @@
     {{-- Header  --}}
     @include('frontend.partials.footer')
 
-    {{-- custom js  --}}
     @stack('js')
 
 </body>
