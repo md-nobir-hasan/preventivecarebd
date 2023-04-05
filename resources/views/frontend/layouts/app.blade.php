93516dbe7b5c048e35b1b0b1f2f3a5cdadff97e9
Md. Nobir Hasan <nobir.wd@gmail.com>
2023-04-05 15:49:39 +0600

    sd

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -3,7 +3,16 @@
 <html dir="ltr" lang="bn">
 
     {{-- Header  --}}
-    @include('frontend.partials.header')
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+        <title>{{ $site_info->title ?? env('APP_NAME') }} </title>
+        <base />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <link href="{{ $site_info->logo }}" rel="icon" />
+        <link rel="stylesheet" href="{{ asset('/css/style.css') }}">
+        @stack('custom-css')
+    </head>
 
 <body class="common-home">
     <div class="toastr-div">
